0b4ea35b0d05a02d1a27673cca2c70a221c54b98
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2026-01-08 20:49:01 +0100

    Cqrs ve Mediator alanları Tamamlandı

---
+++ Core/MaskerBlog.Application/Features/Categories/Handlers/RemoveCategoryCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MaskerBlog.Application.Base;
+using MaskerBlog.Application.Contacts.Persistence;
+using MaskerBlog.Application.Features.Categories.Commands;
+using MaskerBlog.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskerBlog.Application.Features.Categories.Handlers
+{
+    public class RemoveCategoryCommandHandler(IRepository<Category> repository, IUnitOfWork unitOfWork) : 
+        IRequestHandler<RemoveCategoryCommand, BaseResult<bool>>
+    {
+        public async Task<BaseResult<bool>> Handle(RemoveCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = await repository.GetByIdAsync(request.Id);
+            if(category == null)
+            {
+                return BaseResult<bool>.Notfound("Kategori bulunamadı.");
+            }
+
+            repository.Delete(category);
+            var response = await unitOfWork.SaveChangesAsync();
+
+            return response
+                ? BaseResult<bool>.Success(response) 
+                : BaseResult<bool>.Fail("Kategori silinemedi.");
+        }
+    }
+}
---
+++ Core/MaskerBlog.Application/Features/Categories/Validators/UpdateCategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MaskerBlog.Application.Features.Categories.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskerBlog.Application.Features.Categories.Validators
+{
+    public class UpdateCategoryValidator : AbstractValidator<UpdateCategoryCommand>
+    {
+        public UpdateCategoryValidator()
+        {
+            RuleFor(c => c.CategoryName).NotEmpty().WithMessage("Kategori Adı Zorunludur")
+                .MinimumLength(3).WithMessage("Kategori adı en az 3 karakterden oluşmalıdır.");
+
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Kategori Id Gereklidir");
+        }
+    }
+}
