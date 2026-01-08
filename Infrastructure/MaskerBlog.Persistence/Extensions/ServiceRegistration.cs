f00f3bee8a0aa8aa099a345abab85edca6d3a080
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2026-01-08 15:15:55 +0100

    Interceptor işleminde Dbye bagladık

--- Infrastructure/MaskerBlog.Persistence/Extensions/ServiceRegistration.cs
+++ Infrastructure/MaskerBlog.Persistence/Extensions/ServiceRegistration.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using MaskerBlog.Application.Contacts.Persistence;
 using MaskerBlog.Persistence.Concrete;
 using MaskerBlog.Persistence.Context;
+using MaskerBlog.Persistence.Interceptors;
 
 namespace MaskerBlog.Persistence.Extensions
 {
@@ -14,6 +15,7 @@ namespace MaskerBlog.Persistence.Extensions
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString(name: "SqlConnection"));
+                options.AddInterceptors(new AuditDbContextInterceptor());
             });
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
---
+++ Infrastructure/MaskerBlog.Persistence/Interceptors/AuditDbContextInterceptor.cs
@@ -0,0 +1,63 @@
+﻿using MaskerBlog.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskerBlog.Persistence.Interceptors
+{
+    public class AuditDbContextInterceptor : SaveChangesInterceptor 
+    {
+        private static readonly Dictionary<EntityState, Action<DbContext, BaseEntity>> Behaviors = new()
+        {
+            {
+                EntityState.Added, AddedBehavior
+            },
+            {
+                EntityState.Modified, ModifiedBehavior
+            }
+        };
+
+        private static void AddedBehavior(DbContext context, BaseEntity baseEntity)
+        {
+            context.Entry(baseEntity).Property(x => x.UpdatedAt).IsModified = false;
+            baseEntity.CreatedAt = DateTime.Now;
+        }
+
+        private static void ModifiedBehavior(DbContext context, BaseEntity baseEntity)
+        {
+            context.Entry(baseEntity).Property(x => x.CreatedAt).IsModified = false;
+            baseEntity.UpdatedAt = DateTime.Now;
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, 
+            InterceptionResult<int> result, 
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach(var entry in eventData.Context.ChangeTracker.Entries())
+            {
+                if (entry.Entity is not BaseEntity baseEntity)
+                {
+                    continue;
+                }
+
+                if (entry.State is not EntityState.Added and not EntityState.Modified)
+                {
+                    continue;
+                }
+
+                Behaviors[entry.State](eventData.Context, baseEntity);
+            }
+        
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+
+
+
+    }
+}
